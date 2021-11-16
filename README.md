e823c2dbdeac3e08dc0f6964270b4add33df397e
Lori Zurita <lorizurita23@gmail.com>
2021-11-16 15:07:01 -0600

    update with changes to move from mongo to dynamodb all tests updated.

--- README.md
+++ README.md
@@ -1,26 +1,40 @@
-# Pomodoro Todo List (Pomotodo) Backend
+# Pomotodo Backend with DynamoDB
 
 ## Requirements
-
 - node (tested with versions 10+)
-- MongoDB (install locally - can use Docker)
+- AWS account with DynamoDB and IAM role with Programmatic Access
 
 ## Installation
-
 - `npm install`
 
 ## Usage
+1. Create a DynamoDB table called "tododata" with a Primary Key: "tododata" as String
+1. Create a .env file with the following values
+  ```
+  PORT=5000
+  TABLE_NAME=tododata
+  AWS_ACCESS_KEY_ID=******************
+  AWS_SECRET_ACCESS_KEY=****************
+  AWS_DEFAULT_REGION=****************
+  ```
+1. `npm start`
 
-- `npm start`
-- If using a remote MongoDB instance, set the URL in the .env file as the variable **mongoURI** and start the Express server.
+## Exercise
+Two test suites have been created to ensure a reliable refactor of this application from using MongoDB with Mongoose to using the AWS SDK for DynamoDB. The DynamoDB connection has been set up in the "db" directory. 
+1. Begin in the "services" directory, run the Jest test suite with `npm test`, and make your changes in "todoDataService.js". The [AWS SDK DynamoDB Document Client Docs](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html) and [Using Javascript Promises with AWS SDK Requests](https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/using-promises.html) will be most helpful
+1. Work on one test at a time. Use console.log()'s where necessary to see what you're doing. Don't forget to refresh your DynamoDB table in the Management Console to see the Items update there as well.
+1. Once all your service tests are passing, see the "app.test.js" suite in the project's root directory. Remove the "x" before the main "describe" block to enable this test suite. Run `npm test` again and if all goes well, these integration tests should work, too. 
+1. Run a manual test by hooking up your the pomotodo-frontend to use the newly DynamoDB connected backend! 
 
 ## Stories and Acceptance Criteria
-
 ### Example TODO body
 
 ```json
 {
-  "order": ["57f78108-4704-41a9-989b-3721ceedfad1"],
+  "id" : "0"
+  "order": [
+    "57f78108-4704-41a9-989b-3721ceedfad1"
+  ],
   "todos": {
     "57f78108-4704-41a9-989b-3721ceedfad1": {
       "name": "Add entry",
@@ -59,7 +73,7 @@ Request Body:
 ```json
 {
   "name": "Add entry",
-  "desc": "Personal log"
+  "desc": "Personal log",
 }
 ```
 
@@ -67,7 +81,10 @@ Response Body:
 
 ```json
 {
-  "order": ["57f78108-4704-41a9-989b-3721ceedfad1"],
+  "id" : "0"
+  "order": [
+    "57f78108-4704-41a9-989b-3721ceedfad1"
+  ],
   "todos": {
     "57f78108-4704-41a9-989b-3721ceedfad1": {
       "name": "Add entry",
@@ -92,34 +109,33 @@ GET /api/todo-data
 200 OK
 
 Response Body:
-
 ```json
 {
+  "id" : "0"
   "order": [
     "57f78108-4704-41a9-989b-3721ceedfad1",
     "21f3e843-b7c0-4ddb-8df4-0111117bf2d8"
   ],
   "todos": {
-    "57f78108-4704-41a9-989b-3721ceedfad1": {
-      "name": "Add entry",
-      "desc": "Personal log",
-      "dateCreated": "1622077232207",
-      "dateCompleted": "null",
-      "pomodoroCount": 0
-    },
-    "21f3e843-b7c0-4ddb-8df4-0111117bf2d8": {
-      "name": "Rep Building",
-      "desc": "Physical Training",
-      "dateCreated": "1622077232209",
-      "dateCompleted": "null",
-      "pomodoroCount": 0
+      "57f78108-4704-41a9-989b-3721ceedfad1": {
+        "name": "Add entry",
+        "desc": "Personal log",
+        "dateCreated": "1622077232207",
+        "dateCompleted": "null",
+        "pomodoroCount": 0
+      },
+      "21f3e843-b7c0-4ddb-8df4-0111117bf2d8": {
+        "name": "Rep Building",
+        "desc": "Physical Training",
+        "dateCreated": "1622077232209",
+        "dateCompleted": "null",
+        "pomodoroCount": 0
+      }
     }
   }
 }
 ```
-
 ---
-
 As a user I want to update the order of my todos
 
     Given I have saved several todos
@@ -129,10 +145,10 @@ As a user I want to update the order of my todos
 PATCH /api/todo-data
 204 No content
 
-Request Body:
-
+Request Body: 
 ```json
 {
+  "id" : "0"
   "order": [
     "d6b2e215-f095-402e-bf31-f75154a69329",
     "e61cf002-5fa4-41a2-9ace-a7bf4a284d33",
@@ -140,7 +156,6 @@ Request Body:
   ]
 }
 ```
-
 ---
 
 As as user, I want to update a todo when I complete it
@@ -152,14 +167,13 @@ As as user, I want to update a todo when I complete it
 PATCH /api/todo-data/{todo-id}
 204 No Content
 
-Request Body:
 
+Request Body:
 ```json
 {
   "dateCompleted": "1622083278575"
 }
 ```
-
 As a user I want to update a todos name or description
 
     Given I have saved a todo
@@ -169,14 +183,15 @@ As a user I want to update a todos name or description
 PATCH /api/todo-data/{todo-id}
 204 No Content
 
-Request Body:
 
+Request Body:
 ```json
 {
   "name": "WORKOUTMOAR",
-  "desc": "Physical Training"
+  "desc": "Physical Training",
 }
 ```
+
 
 As a user I want to be able to record how many pomodoro intervals I have completed for a todo
 
@@ -187,8 +202,8 @@ As a user I want to be able to record how many pomodoro intervals I have complet
 PATCH /api/todo-data/{todo-id}
 204 No Content
 
-Request Body:
 
+Request Body:
 ```json
 {
   "pomodoroCount": 1
--- services/todoDataService.js
+++ services/todoDataService.js
@@ -1,80 +1,200 @@
+// Calling .config() will allow dotenv to pull environment variables from our .env file...
+require('dotenv').config();
+// ...made available from process.env
+const TableName = process.env.TABLE_NAME;
+// You'll need to call dynamoClient methods to envoke CRUD operations on the DynamoDB table
+const dynamoClient = require('../db');
+// uuid, useful for generating unique ids
 const uuid = require("uuid");
-const TodoData = require("../models/TodoData");
 
 module.exports = class TodoDataService {
   static async addTodo(todo) {
     const id = uuid.v4();
     todo.id = id;
 
-    try {
-      let response;
-      let existingTodoData = await TodoData.find({});
-      if (existingTodoData.length === 0) {
-        const todoData = {};
-        todoData.order = [];
-        todoData.order.push(id);
-        todoData.todos = {};
-        todoData.todos[id] = todo;
+    const params = {
+      TableName, // "tododata"
+    };
 
-        response = await TodoData(todoData).save();
-      } else {
-        existingTodoData = existingTodoData[0];
+    try {
+      // Check the "tododata" table for existing a tododata item
+      //let existingTodoData = ...
+      let existingTodoData = await dynamoClient.scan(params).promise().then((data) => {
+        return data;
+      });
+      
+      // no tododata exists yet
+      if (existingTodoData.Items.length === 0) {
+        const newTodoData = {
+          order: [],
+          todos: {}
+        };
+        newTodoData.id = "0";
+        newTodoData.order.push(id);
+        newTodoData.todos[id] = todo;
+        
+        // Add a new tododata placeholder item to the "tododata" table
+        const params = {
+          TableName,
+          Item: newTodoData,
+        }
+        // ... This is where the PUT goes?
+        await dynamoClient.put(params).promise();
+        
+      } else { // a tododata item already exist
+        existingTodoData = existingTodoData.Items[0];
         existingTodoData.order.push(id);
         existingTodoData.todos[id] = todo;
-
-        response = await TodoData(existingTodoData).save();
+        
+        // Replace the existing tododata item with the new one, created in the above three lines        
+        const params = {
+          TableName,
+          Item: existingTodoData,
+        }
+        // ... Use PUT?
+        await dynamoClient.put(params).promise();
+        // Return the newly created tododata item line 64
       }
-      return response;
+      let newItem = await dynamoClient.scan(params).promise().then((data) => {
+        return data.Items[0];
+      });
+      // Return the newly created tododata item
+      return newItem;
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
 
   static async getTodos() {
     try {
-      const response = await TodoData.findOne({});
-      return response;
+      const params = {
+        TableName,
+        Key: {
+          id: "0"
+        }
+      }
+
+      // Check the "tododata" table for the tododata item, and return it
+      let toDos = await dynamoClient.get(params).promise().then((data) => {
+        return data.Item;
+      });
+      return toDos;
+
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
 
   static async updateOrder(options) {
     try {
-      await TodoData.findOneAndUpdate({}, options);
+      const params = {
+        TableName,
+        Key: {
+          id: "0"
+        },
+        UpdateExpression: "set #order = :newOrder" ,
+          ExpressionAttributeNames: {
+            "#order": "order"
+        },
+        ExpressionAttributeValues: {
+          ":newOrder": options.order,
+        },
+      }
+      // Update the tododata item
+      await dynamoClient.update(params).promise();
+
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
 
   static async updateTodo(id, options) {
     try {
-      let existingTodo = await TodoData.findOne({});
+      let params = {
+        TableName,
+        Key: {
+          id: "0"
+        }
+      }
+
+      // Check the "tododata" table for the tododata item, and set it to "existingTodo"
+      // let existingTodo = ...
+      let existingTodo = await dynamoClient.get(params).promise().then((data) => {
+        return data.Item;
+      });
+
       for (let key in options) {
         existingTodo.todos[id][key] = options[key];
       }
-      await TodoData.findOneAndUpdate({}, existingTodo);
+
+      params = {
+        TableName,
+        Item: {
+          ...existingTodo
+        }
+      }
+
+      // Replace the existing tododata item with the updated one
+      await dynamoClient.put(params).promise();
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
 
   static async deleteTodo(id) {
     try {
-      let existingTodo = await TodoData.findOne({});
+      let params = {
+        TableName,
+        Key: {
+          id: "0"
+        }
+      }
+
+      // Check the "tododata" table for the tododata item, and set it to "existingTodo"
+      let existingTodo = await dynamoClient.get(params).promise().then((data) => {
+        return data.Item;
+      });
+
       existingTodo.order = existingTodo.order.filter((orderId) => {
         return orderId !== id
       });
+
       delete existingTodo.todos[id];
-      await TodoData.findOneAndUpdate({}, existingTodo);
+
+      params = {
+        TableName,
+        Item: {
+          ...existingTodo
+        }
+      }
+
+      
+      // Replace the existing tododata item with the updated one
+      await dynamoClient.put(params).promise();
+
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
 
   static async deleteCompletedTodos() {
     try {
-      let existingTodo = await TodoData.findOne({});
+      let params = {
+        TableName,
+        Key: {
+          id: "0"
+        }
+      }
+
+      let existingTodo = await dynamoClient.scan(params).promise().then((data) => {
+          return data.Items[0];
+      });
+
       existingTodo.order = existingTodo.order.filter((orderId) => {
         return !existingTodo.todos[orderId].completed;
       });
@@ -83,8 +203,17 @@ module.exports = class TodoDataService {
           delete existingTodo.todos[id];
         }
       }
-      await TodoData(existingTodo).save();
+      
+      params = {
+        TableName,
+        Item: {
+          ...existingTodo
+        }
+      }
+
+      await dynamoClient.put(params).promise();
     } catch (error) {
+      console.error(error);
       return error;
     }
   }
